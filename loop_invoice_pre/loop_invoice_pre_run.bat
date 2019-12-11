5bf2c6b2af87cd6e397557312b03060319f75a05
Jen <jen.l357159@gmail.com>
2019-12-10 21:27:49 -0800

    completed project

---
+++ loop_invoice_pre/loop_invoice_pre_run.bat
@@ -0,0 +1,3 @@
+%~d0
+cd %~dp0
+java -Dtalend.component.manager.m2.repository="%cd%/../lib" -Xms256M -Xmx1024M -cp .;../lib/routines.jar;../lib/crypto-utils.jar;../lib/dom4j-1.6.1.jar;../lib/log4j-1.2.17.jar;loop_invoice_pre_0_1.jar; temple_invoice.loop_invoice_pre_0_1.loop_invoice_pre  --context=Default %*
---
+++ task_1/task_1_run.bat
@@ -0,0 +1,3 @@
+%~d0
+cd %~dp0
+java -Dtalend.component.manager.m2.repository="%cd%/../lib" -Xms256M -Xmx1024M -cp .;../lib/routines.jar;../lib/crypto-utils.jar;../lib/dom4j-1.6.1.jar;../lib/log4j-1.2.17.jar;task_1_0_1.jar; temple_invoice.task_1_0_1.task_1  --context=Default %*
